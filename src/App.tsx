a817490ff8fd992367910232917e38b4025ad183
Richard Hull <rm_hull@yahoo.co.uk>
2023-12-23 11:18:56 +0000

    Add about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
-import { Navigate, Route, Routes } from "react-router-dom";
-import Scratch from "./pages/Scratch";
 import { type JSX } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Scratch from "./pages/Scratch";
 
 export default function App(): JSX.Element {
   return (
     <Routes>
-      <Route
-        path="/about"
-        element={
-          <div>
-            <h3>About page</h3>
-          </div>
-        }
-      />
+      <Route path="/about" element={<About />} />
       <Route path="/" element={<Navigate to="/0x00" replace />} />
       <Route path="/:id" element={<Scratch />} />
     </Routes>
