726fad1e57afda324189d450dd9406354f077d98
Richard Hull <rm_hull@yahoo.co.uk>
2023-12-27 00:04:34 +0000

    Add few more file types

--- src/models/fileTypes.ts
+++ src/models/fileTypes.ts
@@ -1,16 +1,19 @@
-import { type Grammar, languages } from "prismjs";
+import { languages, type Grammar } from "prismjs";
 
-import "prismjs/components/prism-json";
-import "prismjs/components/prism-markdown";
-import "prismjs/components/prism-python";
-import "prismjs/components/prism-csv";
 import "prismjs/components/prism-bash";
-import "prismjs/components/prism-regex";
-import "prismjs/components/prism-sql";
-import "prismjs/components/prism-properties";
-import "prismjs/components/prism-yaml";
+import "prismjs/components/prism-csv";
+import "prismjs/components/prism-http";
 import "prismjs/components/prism-javascript";
+import "prismjs/components/prism-json";
+import "prismjs/components/prism-log";
+import "prismjs/components/prism-markdown";
+import "prismjs/components/prism-properties";
+import "prismjs/components/prism-python";
+import "prismjs/components/prism-regex";
+import "prismjs/components/prism-shell-session";
+import "prismjs/components/prism-sql";
 import "prismjs/components/prism-typescript";
+import "prismjs/components/prism-yaml";
 import "./prism-mathjs";
 
 export interface FileType {
@@ -21,16 +24,20 @@ export interface FileType {
 
 export const supportedTypes: readonly FileType[] = [
   { language: "bash", descr: "Bash", grammar: languages.bash },
+  { language: "css", descr: "CSS", grammar: languages.css },
   { language: "csv", descr: "CSV", grammar: languages.csv },
   { language: "html", descr: "HTML", grammar: languages.html },
+  { language: "http", descr: "HTTP", grammar: languages.http },
   { language: "javascript", descr: "Javascript", grammar: languages.javascript },
   { language: "json", descr: "JSON", grammar: languages.json },
+  { language: "log", descr: "Logs", grammar: languages.log },
   { language: "markdown", descr: "Markdown", grammar: languages.markdown },
   { language: "math.js", descr: "Math.JS", grammar: languages.mathjs },
   { language: "properties", descr: "Properties", grammar: languages.properties },
   { language: "python", descr: "Python", grammar: languages.python },
   { language: "sql", descr: "SQL", grammar: languages.sql },
   { language: "regex", descr: "Regex", grammar: languages.regex },
+  { language: "shell-session", descr: "Shell", grammar: languages["shell-session"] },
   { language: "text", descr: "Text", grammar: languages.text },
   { language: "typescript", descr: "Typescript", grammar: languages.typescript },
   { language: "yaml", descr: "YAML", grammar: languages.yaml },
